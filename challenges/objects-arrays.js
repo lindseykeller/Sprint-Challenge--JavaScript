8e46d6afc460bcee14bd134a51bf0e0b1815797f
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-01-27 22:10:11 -0800

    unisWithUni function returning booleans instead of string

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -92,7 +92,11 @@ const contactInfo  = graduates.map((grad) => {
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+const unisWithUni = graduates.map((grad) => {
+ return grad.university.includes('Uni');
+
+})
+
 console.log(unisWithUni);
 
 
